b9841d6a5c1fc73650367722c94df3130ae8c555
Rieqy Erysya <rieqyns13@gmail.com>
2019-03-01 03:26:36 +0700

    update

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_9b4666b37f7da47'),
+            'username' => env('DB_USERNAME', 'ba20eaf945586e'),
+            'password' => env('DB_PASSWORD', 'decd734c'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',

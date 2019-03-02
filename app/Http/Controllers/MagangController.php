3e9562a6ba63d5cee11126baf1f1a1faec9a0b8d
Rieqy Erysya <rieqyns13@gmail.com>
2019-03-02 21:06:35 +0700

    'asu'

--- app/Http/Controllers/MagangController.php
+++ app/Http/Controllers/MagangController.php
@@ -173,27 +173,27 @@ class MagangController extends Controller
             $query->where('user_id',auth()->user()->id);
         })->with('users')->findOrFail($magang_id);
 
-        $nama = $magang->users->name;
-        $gambar = asset('assets/images/sertifikat.jpg');
-        //return $gambar;
-        $image = imagecreatefromjpeg($gambar);
-        $white = imageColorAllocate($image, 255, 255, 255);
-        $black = imageColorAllocate($image, 0, 0, 0);
-        $font = "D:\BRUSHSCI.TTF"; 
-        $size = 50;
-        //definisikan lebar gambar agar posisi teks selalu ditengah berapapun jumlah hurufnya
-        $image_width = imagesx($image);  
-        //membuat textbox agar text centered
-        $text_box = imagettfbbox($size,0,$font,$nama);
-        $text_width = $text_box[2]-$text_box[0]; // lower right corner - lower left corner
-        $text_height = $text_box[3]-$text_box[1];
-        $x = ($image_width/2) - ($text_width/2);
-        //generate sertifikat beserta namanya
-        imagettftext($image, $size, 0, $x, 430, $black, $font, $nama);
-        //tampilkan di browser
-        header("Content-type:  image/jpeg");
-        imagejpeg($image);
-        imagedestroy($image);
+        // $nama = $magang->users->name;
+        // $gambar = asset('assets/images/sertifikat.jpg');
+        // //return $gambar;
+        // $image = imagecreatefromjpeg($gambar);
+        // $white = imageColorAllocate($image, 255, 255, 255);
+        // $black = imageColorAllocate($image, 0, 0, 0);
+        // $font = "D:\BRUSHSCI.TTF"; 
+        // $size = 50;
+        // //definisikan lebar gambar agar posisi teks selalu ditengah berapapun jumlah hurufnya
+        // $image_width = imagesx($image);  
+        // //membuat textbox agar text centered
+        // $text_box = imagettfbbox($size,0,$font,$nama);
+        // $text_width = $text_box[2]-$text_box[0]; // lower right corner - lower left corner
+        // $text_height = $text_box[3]-$text_box[1];
+        // $x = ($image_width/2) - ($text_width/2);
+        // //generate sertifikat beserta namanya
+        // imagettftext($image, $size, 0, $x, 430, $black, $font, $nama);
+        // //tampilkan di browser
+        // header("Content-type:  image/jpeg");
+        // imagejpeg($image);
+        // imagedestroy($image);
         
 
     }
